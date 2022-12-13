f5a3bb7eb5a2b14e2cb78f79fca1c8ab3d34db68
alikeshtkar <alikeshtkar262@gmail.com>
2022-12-13 09:30:12 +0330

    add server swagger 2

--- Modules/Core/Foundation/Api/SwaggerBase.php
+++ Modules/Core/Foundation/Api/SwaggerBase.php
@@ -5,7 +5,7 @@ namespace Modules\Core\Foundation\Api;
 use OpenApi\Annotations as OA;
 
 /**
- * @OA\Server(url="http://ario.jcoders.ir/api")
+ * @OA\Server(url="https://ario.jcoders.ir/api")
  * @OA\Server(url="http://127.0.0.1:8000/api/v1")
  * @OA\Info(
  *     version="1.0",
