3d8a48624a0347f5088f290408aca67ef92e8510
alikeshtkar <alikeshtkar262@gmail.com>
2023-01-03 17:32:30 +0330

    fresh 15

--- Modules/Product/Entities/Product.php
+++ Modules/Product/Entities/Product.php
@@ -339,7 +339,7 @@ class Product extends Model
      */
     public function store($request): Model|Builder
     {
-        dd("sala");
+        dd($request->all());
         return DB::transaction(function () use ($request) {
             $product = self::query()->create([
                 'user_id' => $request->user()->id,
