1937d7787df8067785de59b2f19d9759a70a4286
alikeshtkar <alikeshtkar262@gmail.com>
2023-01-28 10:34:17 +0330

    push

--- Modules/Vote/Http/Controllers/V1/Api/ApiVoteController.php
+++ Modules/Vote/Http/Controllers/V1/Api/ApiVoteController.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Modules\Core\Responses\Api\ApiResponse;
+use Modules\Core\Transformers\Api\ApiPaginationResource;
 use Modules\Vote\Entities\Vote;
 use Modules\Vote\Transformers\Api\Admin\ApiAdminVoteResource;
+use Modules\Vote\Transformers\Api\ApiVoteResource;
 
 class ApiVoteController extends Controller
 {
@@ -22,8 +24,10 @@ class ApiVoteController extends Controller
             ->withRelationships(['items' => function ($q) {
 
             }])->paginateAdmin($request);
+        $resource = ApiPaginationResource::make($votes)->additional(['itemsResource' => ApiVoteResource::class]);
+
         return ApiResponse::message(trans("Received information successfully"))
-            ->addData('vote', ApiAdminVoteResource::collection($votes))
+            ->addData('vote', $resource)
             ->send();
     }
 }
