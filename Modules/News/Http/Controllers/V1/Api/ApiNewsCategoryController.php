9150148e498664fcddc610f76db2df98aa52a430
alikeshtkar <alikeshtkar262@gmail.com>
2023-01-28 10:57:20 +0330

    push

--- Modules/News/Http/Controllers/V1/Api/ApiNewsCategoryController.php
+++ Modules/News/Http/Controllers/V1/Api/ApiNewsCategoryController.php
@@ -16,8 +16,7 @@ class ApiNewsCategoryController extends Controller
     public function index(Request $request, $newsCategory = null)
     {
         if ($newsCategory) $newsCategory = NewsCategory::init()->selectColumns(['id'])->findOrFailById($newsCategory)->id;
-        $newsCategories = NewsCategory::init()->selectColumns(['id', 'title'])
-            ->withScopes(['accepted'])
+        $newsCategories = NewsCategory::init()->selectColumns(['id', 'title', 'parent_id'])
             ->getIndex($request, $newsCategory);
         return ApiResponse::message(trans("Received information successfully"))
             ->addData('newCategories', $newsCategories)
