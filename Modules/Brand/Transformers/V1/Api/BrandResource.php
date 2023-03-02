990f8046ef1ab371f635bd279c16406d4c0a3449
alikeshtkar <alikeshtkar262@gmail.com>
2023-03-03 01:34:12 +0330

    push

--- Modules/Brand/Transformers/V1/Api/BrandResource.php
+++ Modules/Brand/Transformers/V1/Api/BrandResource.php
@@ -4,6 +4,7 @@ namespace Modules\Brand\Transformers\V1\Api;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Collection;
+use Modules\Brand\Enums\BrandStatus;
 
 class BrandResource extends JsonResource
 {
@@ -15,8 +16,10 @@ class BrandResource extends JsonResource
      */
     public function toArray($request)
     {
-        return collect($this->resource)
-            ->when($this->resource->relationLoaded('image'), function (Collection $collection) {
+        return collect($this->resource)->when(array_key_exists('status',$this->resource->toArray()),function (Collection $collection){
+          $collection->put('translated_status',BrandStatus::getDescription($this->resource->status))
+          ->put('status_css_class',BrandStatus::fromValue($this->resource->status)->getCssClass());
+        })->when($this->resource->relationLoaded('image'), function (Collection $collection) {
                 $collection->put('image', $this->resource->image);
             });
     }
