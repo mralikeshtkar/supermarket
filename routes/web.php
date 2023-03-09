23021a5651d4b536ca756f2b973a41eaa851208b
alikeshtkar <alikeshtkar262@gmail.com>
2023-03-09 20:06:49 +0330

    push

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    phpinfo();
     $p=\Modules\Product\Entities\Product::query()->inRandomOrder()->first()->setDirectory('products')
         ->setCollection(config('product.collection_gallery'))
         ->setPriority(1)
