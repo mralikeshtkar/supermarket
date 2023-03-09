d5adad24532cef72dab3a8e9c7153cc6255bd2b8
alikeshtkar <alikeshtkar262@gmail.com>
2023-03-09 20:15:14 +0330

    push

--- Modules/Media/Services/ImageStorageService.php
+++ Modules/Media/Services/ImageStorageService.php
@@ -15,7 +15,7 @@ class ImageStorageService extends BaseStorageService implements Interfaces\FileS
 
     private function resizeImage($disk, $file, $directory, $size): bool|string
     {
-        dd($file);
+        dd(Image::make($file));
         /** @var UploadedFile $file */
         $image = Image::make($file);
         $img = $image->resize(
