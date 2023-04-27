f64896556a6e4322c4c57e54d9ac3217375298b9
mralikeshtkar <alikeshtkar262@gmail.com>
2023-04-27 23:31:48 +0330

    123

--- Modules/Rack/Entities/Rack.php
+++ Modules/Rack/Entities/Rack.php
@@ -170,9 +170,9 @@ class Rack extends Model
     public function allRackRowsWithProductIds()
     {
         return self::query()
-            ->select(['id','priority'])
+            ->select(['id','priority','status'])
             ->with(['rows' => function (HasMany $hasMany) {
-                $hasMany->select(['id','rack_id','priority'])->withWhereHas('products', function ($builder) {
+                $hasMany->select(['id','rack_id','priority','status'])->withWhereHas('products', function ($builder) {
                     $builder->select(['products.id'])->accepted();
                 })->orderByPriorityAsc()->active();
             }])->orderByPriorityAsc()
