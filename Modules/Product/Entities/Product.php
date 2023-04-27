de72f19be0236b3eb8baf13f2b76219497463127
mralikeshtkar <alikeshtkar262@gmail.com>
2023-04-27 23:26:41 +0330

    123

--- Modules/Product/Entities/Product.php
+++ Modules/Product/Entities/Product.php
@@ -216,8 +216,10 @@ class Product extends Model
             ->paginate();
         $products->setCollection($products->getCollection()->transform(function ($item) use ($racks) {
             /** @var Collection $racks */
-            $item->rows =$racks->first(function ($rack){
-                dd($rack);
+            $item->rows = $racks->first(function ($rack) use ($item) {
+                return $rack->rows->first(function ($row) use ($item) {
+                    return $row->products->count() && $row->products->contains($item->id);
+                });
             });
             return $item;
         }));
