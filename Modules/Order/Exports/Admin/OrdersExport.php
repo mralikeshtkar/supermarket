08f0b1f30471917f6605bd7dcf16a1611560dc77
mralikeshtkar <alikeshtkar262@gmail.com>
2023-05-05 16:19:53 +0330

    123

--- Modules/Order/Exports/Admin/OrdersExport.php
+++ Modules/Order/Exports/Admin/OrdersExport.php
@@ -9,6 +9,7 @@ use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Modules\Order\Entities\Order;
+use Modules\Order\Enums\OrderStatus;
 use Modules\User\Entities\User;
 
 class OrdersExport implements FromQuery, WithMapping, WithColumnFormatting, WithHeadings, ShouldAutoSize
@@ -19,6 +20,7 @@ class OrdersExport implements FromQuery, WithMapping, WithColumnFormatting, With
 
     public function map($row): array
     {
+
         return [
             $row->id,
             $row->address->name,
@@ -26,6 +28,7 @@ class OrdersExport implements FromQuery, WithMapping, WithColumnFormatting, With
             number_format($row->total),
             number_format($row->discount),
             number_format($row->amount),
+            OrderStatus::getDescription($this->status),
             verta($row->created_at)->formatJalaliDatetime(),
         ];
     }
@@ -46,6 +49,7 @@ class OrdersExport implements FromQuery, WithMapping, WithColumnFormatting, With
             __('Total'),
             __('Discount'),
             __('Money Paid'),
+            __('Status'),
             __('Date'),
         ];
     }
@@ -54,6 +58,7 @@ class OrdersExport implements FromQuery, WithMapping, WithColumnFormatting, With
     {
         return Order::query()
             ->with(['address','invoices'])
+            ->latest()
             ->when($this->request, function ($q) {
                 $q->filter($this->request);
             });
