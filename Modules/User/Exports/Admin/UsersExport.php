1c72015369d5b47eeb8c70bdc1c5a5282f181ba5
mralikeshtkar <alikeshtkar262@gmail.com>
2023-05-05 14:06:31 +0330

    123

--- Modules/User/Exports/Admin/UsersExport.php
+++ Modules/User/Exports/Admin/UsersExport.php
@@ -4,14 +4,21 @@ namespace Modules\User\Exports\Admin;
 
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Modules\User\Entities\User;
 
-class UsersExport implements FromCollection
+class UsersExport implements FromCollection,WithHeadings
 {
     public function collection()
     {
         return User::query()
+            ->select(['id','name','mobile'])
             ->limit(10)
             ->get();
+    }
+
+    public function headings(): array
+    {
+        return ['id','name','mobile'];
     }
 }
