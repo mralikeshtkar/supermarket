d3c73ca4fe49bd076f84cf8c301d6f15c1c5d68d
mralikeshtkar <alikeshtkar262@gmail.com>
2023-05-05 13:51:04 +0330

    123

---
+++ Modules/User/Exports/Admin/UsersExport.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modules\User\Exports\Admin;
+
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Modules\User\Entities\User;
+
+class UsersExport implements FromCollection
+{
+
+    public function collection()
+    {
+        return User::query()
+            ->limit(10)
+            ->get();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,30 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $p=\Modules\Product\Entities\Product::query()->inRandomOrder()->first()->setDirectory('products')
-        ->setCollection(config('product.collection_gallery'))
-        ->setPriority(1)
-        ->addMedia(\Illuminate\Http\UploadedFile::fake()->create('test.png'));
-    dd($p);
-    dd(\Intervention\Image\Facades\Image::make(public_path('storage/advertisements/63d41803cf5e91674844163.jpg')));
-    $files = \Illuminate\Support\Facades\File::files(public_path('storage/factors'));
-    foreach ($files as $file) {
-        if (!now()->lt(\Illuminate\Support\Carbon::createFromTimestamp($file->getMTime())->addMinutes(30))){
-            File::delete($file->getPathname());
-        }
-    }
-    dd("ok");
-    return collect(range(1, 16))->map(function ($item) {
-        return "https://cdna.p30download.ir/p30dl-tutorial/Udemy.React.Js.With.Laravel.Build.Complete.PWA.Ecommerce.Project-p30download.com.part" . ($item < 10 ? "0" . $item : $item) . ".rar";
-    })->implode("<br/>");
-    $data = [
-        'name' => 'علی'
-    ];
-//    return view('factor',$data);
-    $pdf = PDF::loadView('factor', $data, [], [
-        'format' => [685.98, 396.85],
-    ]);
-    return $pdf->download(now()->toDateTimeString() . '-test.pdf');
-    return \niklasravnsborg\LaravelPdf\Facades\Pdf::loadView('factor', ['name' => "علی"])->download('test.pdf');
-    return view('welcome');
+    return \Maatwebsite\Excel\Facades\Excel::download(new \Modules\User\Exports\Admin\UsersExport(),'users-'.now().'.xlsx');
 });
