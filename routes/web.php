e78bca833c6aa320ca6d7ba98211152a58ea7714
mralikeshtkar <alikeshtkar262@gmail.com>
2023-05-05 14:28:32 +0330

    123

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return \Maatwebsite\Excel\Facades\Excel::download(new \Modules\User\Exports\Admin\UsersExport(),'users-'.now().'.xlsx');
+    return \Maatwebsite\Excel\Facades\Excel::download(new \Modules\User\Exports\Admin\UsersExport(),'users-'.verta().'.xlsx');
 });
